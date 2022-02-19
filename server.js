f942137b6e2e9c52cc09eff6c3bf3d358657b05f
Nathaniel Boucher <93409883+natejboucher@users.noreply.github.com>
2022-02-18 23:52:44 -0500

    added get and post requests for HTML and note saving

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const { notes } = require('./db/db.json');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.static('public'));
+app.use(express.json());
+
+
+// Send html files to the client
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+// Display notes api
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
+
+app.post("/api/notes", (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = notes.length.toString();
+
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+});
+
+function createNewNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify({ notes: notesArray }, null, 2)
+    );
+    return note;
+}
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
